5eb4474d21db6a199f3d0fb6a532b5471970c365
wyj2046 <wyj2046@gmail.com>
2017-03-28 14:36:07 +0800

    def func

--- feature.py
+++ feature.py
@@ -15,7 +15,7 @@ import sys
 stop_words = stopwords.words('english')
 
 
-def wmd(s1, s2):
+def wmd(model, s1, s2):
     s1 = str(s1).lower().decode('utf-8').split()
     s2 = str(s2).lower().decode('utf-8').split()
     s1 = [w for w in s1 if w not in stop_words]
@@ -23,7 +23,7 @@ def wmd(s1, s2):
     return model.wmdistance(s1, s2)
 
 
-def norm_wmd(s1, s2):
+def norm_wmd(model, s1, s2):
     s1 = str(s1).lower().decode('utf-8').split()
     s2 = str(s2).lower().decode('utf-8').split()
     s1 = [w for w in s1 if w not in stop_words]
@@ -45,15 +45,8 @@ def sent2vec(s):
     v = M.sum(axis=0)
     return v / np.sqrt((v ** 2).sum())
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print "usage:%s input_file output_file" % sys.argv[0]
-        sys.exit(-1)
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
 
-    data = pd.read_csv(input_file)
-
+def add_basic_feat(data):
     data['len_q1'] = data['question1'].apply(lambda x: len(str(x)))
     data['len_q2'] = data['question2'].apply(lambda x: len(str(x)))
     data['diff_len'] = data['len_q1'] - data['len_q2']
@@ -63,6 +56,8 @@ if __name__ == '__main__':
     data['len_word_q2'] = data['question2'].apply(lambda x: len(str(x).split()))
     data['common_words'] = data.apply(lambda x: len(set(str(x['question1']).lower().split()).intersection(set(str(x['question2']).lower().split()))), axis=1)
 
+
+def add_fuzz_feat(data):
     data['fuzz_qratio'] = data.apply(lambda x: fuzz.QRatio(str(x['question1']), str(x['question2'])), axis=1)
     data['fuzz_wratio'] = data.apply(lambda x: fuzz.WRatio(str(x['question1']), str(x['question2'])), axis=1)
     data['fuzz_partial_ratio'] = data.apply(lambda x: fuzz.partial_ratio(str(x['question1']), str(x['question2'])), axis=1)
@@ -71,12 +66,14 @@ if __name__ == '__main__':
     data['fuzz_token_set_ratio'] = data.apply(lambda x: fuzz.token_set_ratio(str(x['question1']), str(x['question2'])), axis=1)
     data['fuzz_token_sort_ratio'] = data.apply(lambda x: fuzz.token_sort_ratio(str(x['question1']), str(x['question2'])), axis=1)
 
+
+def add_word2vec_feat(data):
     model = gensim.models.KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin.gz', binary=True)
-    data['wmd'] = data.apply(lambda x: wmd(x['question1'], x['question2']), axis=1)
+    data['wmd'] = data.apply(lambda x: wmd(model, x['question1'], x['question2']), axis=1)
 
     norm_model = gensim.models.KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin.gz', binary=True)
     norm_model.init_sims(replace=True)
-    data['norm_wmd'] = data.apply(lambda x: norm_wmd(x['question1'], x['question2']), axis=1)
+    data['norm_wmd'] = data.apply(lambda x: norm_wmd(norm_model, x['question1'], x['question2']), axis=1)
 
     question1_vectors = np.zeros((data.shape[0], 300))
     for i, q in tqdm(enumerate(data['question1'].values)):
@@ -85,6 +82,9 @@ if __name__ == '__main__':
     question2_vectors = np.zeros((data.shape[0], 300))
     for i, q in tqdm(enumerate(data['question2'].values)):
         question2_vectors[i, :] = sent2vec(q)
+
+    # cPickle.dump(question1_vectors, open('data/q1_w2v.pkl', 'wb'), -1)
+    # cPickle.dump(question2_vectors, open('data/q2_w2v.pkl', 'wb'), -1)
 
     data['cosine_distance'] = [cosine(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors), np.nan_to_num(question2_vectors))]
     data['cityblock_distance'] = [cityblock(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors), np.nan_to_num(question2_vectors))]
@@ -99,7 +99,17 @@ if __name__ == '__main__':
     data['kur_q1vec'] = [kurtosis(x) for x in np.nan_to_num(question1_vectors)]
     data['kur_q2vec'] = [kurtosis(x) for x in np.nan_to_num(question2_vectors)]
 
-    # cPickle.dump(question1_vectors, open('data/q1_w2v.pkl', 'wb'), -1)
-    # cPickle.dump(question2_vectors, open('data/q2_w2v.pkl', 'wb'), -1)
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print "usage:%s input_file output_file" % sys.argv[0]
+        sys.exit(-1)
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    data = pd.read_csv(input_file)
+
+    add_basic_feat(data)
+    add_fuzz_feat(data)
+    add_word2vec_feat(data)
 
     data.to_csv(output_file, index=False)
