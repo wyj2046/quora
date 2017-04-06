925eaa96b2af3e0a2812fc25c78dee701f323a5e
wyj2046 <wyj2046@gmail.com>
2017-04-06 20:06:35 +0800

    add deepnet_feature.py

--- deepnet2.py
+++ deepnet2.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
-import sys
+import argparse
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
-from keras.preprocessing import text
-from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers.embeddings import Embedding
 from keras.layers.core import Dense
@@ -17,78 +14,126 @@ from keras.callbacks import ModelCheckpoint
 from keras.callbacks import EarlyStopping
 
 
-MAX_NUM_WORDS = 200000
-EMBEDDING_DIM = 300
-MAX_SEQUENCE_LENGTH = 25
-BATCH_SIZE = 384
-NB_EPOCH = 25
-VALIDATION_SPLIT = 0.1
-
-
 if __name__ == '__main__':
-    # python deepnet2.py input/train.csv input/test.csv model/deepnet2.h5 submission/deepnet2.csv data/glove.840B.300d.txt plot/history2.csv
-    if len(sys.argv) != 7:
-        print 'usage:%s train test model submission glove'
-        sys.exit(-1)
-    train_file = sys.argv[1]
-    test_file = sys.argv[2]
-    model_file = sys.argv[3]
-    submission_file = sys.argv[4]
-    glove_file = sys.argv[5]
-    history_file = sys.argv[6]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--train_q1',
+        help='train_q1 data file',
+        required=True
+    )
+    parser.add_argument(
+        '--train_q2',
+        help='train_q2 data file',
+        required=True
+    )
+    parser.add_argument(
+        '--test_q1',
+        help='test_q1 data file',
+        required=True
+    )
+    parser.add_argument(
+        '--test_q2',
+        help='test_q2 data file',
+        required=True
+    )
+    parser.add_argument(
+        '--labels',
+        help='labels data file',
+        required=True
+    )
+    parser.add_argument(
+        '--test_id',
+        help='test_id data file',
+        required=True
+    )
+    parser.add_argument(
+        '--embedding_matrix',
+        help='embedding_matrix data file',
+        required=True
+    )
+    parser.add_argument(
+        '--model_file',
+        help='model_file',
+        required=True
+    )
+    parser.add_argument(
+        '--submission_file',
+        help='submission_file',
+        required=True
+    )
+    parser.add_argument(
+        '--history_file',
+        help='history_file',
+        required=True
+    )
+    parser.add_argument(
+        '--nb_words',
+        help='nb_words',
+        type=int,
+        required=True
+    )
+    parser.add_argument(
+        '--embedding_dim',
+        help='embedding_dim',
+        type=int,
+        default=300
+    )
+    parser.add_argument(
+        '--max_sequence_length',
+        help='max_sequence_length',
+        type=int,
+        default=25
+    )
+    parser.add_argument(
+        '--batch_size',
+        help='batch_size',
+        type=int,
+        default=384
+    )
+    parser.add_argument(
+        '--nb_epoch',
+        help='nb_epoch',
+        type=int,
+        default=25
+    )
+    parser.add_argument(
+        '--validation_split',
+        help='validation_split',
+        type=float,
+        default=0.1
+    )
 
-    train_data = pd.read_csv(train_file)
-    test_data = pd.read_csv(test_file)
+    args = parser.parse_args()
 
-    train_y = train_data.is_duplicate.values
+    train_q1 = np.load(open(args.train_q1, 'rb'))
+    train_q2 = np.load(open(args.train_q2, 'rb'))
+    test_q1 = np.load(open(args.test_q1, 'rb'))
+    test_q2 = np.load(open(args.test_q2, 'rb'))
 
-    tk = text.Tokenizer(num_words=MAX_NUM_WORDS)
-    tk.fit_on_texts(list(train_data.question1.values.astype(str)) +
-                    list(train_data.question2.values.astype(str)) +
-                    list(test_data.question1.values.astype(str)) +
-                    list(train_data.question1.values.astype(str)))
+    labels = np.load(open(args.labels, 'rb'))
+    train_y = labels
 
-    word_index = tk.word_index
+    test_id = np.load(open(args.test_id, 'rb'))
 
-    embeddings_index = {}
-    f = open(glove_file)
-    for line in tqdm(f):
-        values = line.split()
-        word = values[0]
-        embedding = np.asarray(values[1:], dtype='float32')
-        embeddings_index[word] = embedding
-    f.close()
+    embedding_matrix = np.load(open(args.embedding_matrix, 'rb'))
 
-    nb_words = min(MAX_NUM_WORDS, len(word_index))
-
-    embedding_matrix = np.zeros((nb_words + 1, EMBEDDING_DIM))
-    for word, i in tqdm(word_index.items()):
-        if i > MAX_NUM_WORDS:
-            continue
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:
-            embedding_matrix[i] = embedding_vector
-
-    train_q1_word_sequence = tk.texts_to_sequences(train_data.question1.values.astype(str))
-    train_q2_word_sequence = tk.texts_to_sequences(train_data.question2.values.astype(str))
-    test_q1_word_sequence = tk.texts_to_sequences(test_data.question1.values.astype(str))
-    test_q2_word_sequence = tk.texts_to_sequences(test_data.question2.values.astype(str))
-
-    train_q1 = sequence.pad_sequences(train_q1_word_sequence, maxlen=MAX_SEQUENCE_LENGTH)
-    train_q2 = sequence.pad_sequences(train_q2_word_sequence, maxlen=MAX_SEQUENCE_LENGTH)
-    test_q1 = sequence.pad_sequences(test_q1_word_sequence, maxlen=MAX_SEQUENCE_LENGTH)
-    test_q2 = sequence.pad_sequences(test_q2_word_sequence, maxlen=MAX_SEQUENCE_LENGTH)
+    nb_words = args.nb_words
+    embedding_dim = args.embedding_dim
+    max_sequence_length = args.max_sequence_length
+    batch_size = args.batch_size
+    nb_epoch = args.nb_epoch
+    validation_split = args.validation_split
 
     Q1 = Sequential()
-    Q1.add(Embedding(nb_words + 1, EMBEDDING_DIM, weights=[embedding_matrix], input_length=MAX_SEQUENCE_LENGTH, trainable=False))
+    Q1.add(Embedding(nb_words + 1, embedding_dim, weights=[embedding_matrix], input_length=max_sequence_length, trainable=False))
 
-    Q1.add(TimeDistributed(Dense(EMBEDDING_DIM, activation='relu')))
-    Q1.add(Lambda(lambda x: K.max(x, axis=1), output_shape=(EMBEDDING_DIM, )))
+    Q1.add(TimeDistributed(Dense(embedding_dim, activation='relu')))
+    Q1.add(Lambda(lambda x: K.max(x, axis=1), output_shape=(embedding_dim, )))
 
     Q2 = Sequential()
-    Q2.add(Embedding(nb_words + 1, EMBEDDING_DIM, weights=[embedding_matrix], input_length=MAX_SEQUENCE_LENGTH, trainable=False))
-    Q2.add(TimeDistributed(Dense(EMBEDDING_DIM, activation='relu')))
-    Q2.add(Lambda(lambda x: K.max(x, axis=1), output_shape=(EMBEDDING_DIM, )))
+    Q2.add(Embedding(nb_words + 1, embedding_dim, weights=[embedding_matrix], input_length=max_sequence_length, trainable=False))
+    Q2.add(TimeDistributed(Dense(embedding_dim, activation='relu')))
+    Q2.add(Lambda(lambda x: K.max(x, axis=1), output_shape=(embedding_dim, )))
 
     model = Sequential()
     model.add(Merge([Q1, Q2], mode='concat'))
@@ -102,19 +147,19 @@ if __name__ == '__main__':
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    checkpoint = ModelCheckpoint(model_file, monitor='val_acc', save_best_only=True, verbose=1)
+    checkpoint = ModelCheckpoint(args.model_file, monitor='val_acc', save_best_only=True, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=50, verbose=0, mode='auto')
 
-    history = model.fit([train_q1, train_q2], y=train_y, batch_size=BATCH_SIZE, epochs=NB_EPOCH, verbose=1, validation_split=VALIDATION_SPLIT, shuffle=True, callbacks=[checkpoint, early_stopping])
+    history = model.fit([train_q1, train_q2], y=train_y, batch_size=batch_size, epochs=nb_epoch, verbose=1, validation_split=validation_split, shuffle=True, callbacks=[checkpoint, early_stopping])
 
-    model.load_weights(model_file)
+    model.load_weights(args.model_file)
 
-    pred = model.predict([test_q1, test_q2], batch_size=BATCH_SIZE, verbose=1)
+    pred = model.predict([test_q1, test_q2], batch_size=batch_size, verbose=1)
 
     sub = pd.DataFrame()
-    sub['test_id'] = test_data['test_id'].values
+    sub['test_id'] = test_id
     sub['is_duplicate'] = pred
-    sub.to_csv(submission_file, index=False)
+    sub.to_csv(args.submission_file, index=False)
 
     min_val_loss, idx = min((val, idx) for (idx, val) in enumerate(history.history['val_loss']))
     print idx + 1, min_val_loss
@@ -122,7 +167,7 @@ if __name__ == '__main__':
     history_pd = pd.DataFrame({'epoch': [i + 1 for i in history.epoch],
                                'training': history.history['loss'],
                                'validation': history.history['val_loss']})
-    history_pd.to_csv(history_file, index=False)
+    history_pd.to_csv(args.history_file, index=False)
     # plot
     # ax = history.ix[:, :].plot(x='epoch', figsize={5, 8}, grid=True)
     # ax.set_ylabel('loss')
---
+++ deepnet_feature.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import json
+from keras.preprocessing import text
+from keras.preprocessing import sequence
+
+
+MAX_NUM_WORDS = 200000
+EMBEDDING_DIM = 300
+MAX_SEQUENCE_LENGTH = 25
+
+
+TRAIN_FILE = 'input/train.csv'
+TEST_FILE = 'input/test.csv'
+GLOVE_FILE = 'data/glove.840B.300d.txt'
+
+TRAIN_Q1_DATA_FILE = 'data/train_q1.npy'
+TRAIN_Q2_DATA_FILE = 'data/train_q2.npy'
+TEST_Q1_DATA_FILE = 'data/test_q1.npy'
+TEST_Q2_DATA_FILE = 'data/test_q2.npy'
+EMBEDDING_MATRIX_FILE = 'data/embedding_matrix.npy'
+NB_WORDS_DATA_FILE = 'data/nb_words.json'
+LABEL_FILE = 'data/label.npy'
+TEST_ID_FILE = 'data/test_id.npy'
+
+
+if __name__ == '__main__':
+    train_data = pd.read_csv(TRAIN_FILE)
+    test_data = pd.read_csv(TEST_FILE)
+
+    train_y = train_data.is_duplicate.values
+
+    tk = text.Tokenizer(num_words=MAX_NUM_WORDS)
+    tk.fit_on_texts(list(train_data.question1.values.astype(str)) +
+                    list(train_data.question2.values.astype(str)) +
+                    list(test_data.question1.values.astype(str)) +
+                    list(train_data.question1.values.astype(str)))
+
+    word_index = tk.word_index
+
+    embeddings_index = {}
+    f = open(GLOVE_FILE)
+    for line in tqdm(f):
+        values = line.split()
+        word = values[0]
+        embedding = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = embedding
+    f.close()
+
+    nb_words = min(MAX_NUM_WORDS, len(word_index))
+
+    embedding_matrix = np.zeros((nb_words + 1, EMBEDDING_DIM))
+    for word, i in tqdm(word_index.items()):
+        if i > MAX_NUM_WORDS:
+            continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+    train_q1_word_sequence = tk.texts_to_sequences(train_data.question1.values.astype(str))
+    train_q2_word_sequence = tk.texts_to_sequences(train_data.question2.values.astype(str))
+    test_q1_word_sequence = tk.texts_to_sequences(test_data.question1.values.astype(str))
+    test_q2_word_sequence = tk.texts_to_sequences(test_data.question2.values.astype(str))
+
+    train_q1 = sequence.pad_sequences(train_q1_word_sequence, maxlen=MAX_SEQUENCE_LENGTH)
+    train_q2 = sequence.pad_sequences(train_q2_word_sequence, maxlen=MAX_SEQUENCE_LENGTH)
+    test_q1 = sequence.pad_sequences(test_q1_word_sequence, maxlen=MAX_SEQUENCE_LENGTH)
+    test_q2 = sequence.pad_sequences(test_q2_word_sequence, maxlen=MAX_SEQUENCE_LENGTH)
+
+    np.save(open(TRAIN_Q1_DATA_FILE, 'wb'), train_q1)
+    np.save(open(TRAIN_Q2_DATA_FILE, 'wb'), train_q2)
+    np.save(open(TEST_Q1_DATA_FILE, 'wb'), test_q1)
+    np.save(open(TEST_Q2_DATA_FILE, 'wb'), test_q2)
+
+    np.save(open(EMBEDDING_MATRIX_FILE, 'wb'), embedding_matrix)
+
+    with open(NB_WORDS_DATA_FILE, 'w') as f:
+        json.dump({'nb_words': nb_words}, f)
+
+    labels = np.array(train_data.is_duplicate.values, dtype=int)
+    np.save(open(LABEL_FILE, 'wb'), labels)
+
+    test_id = np.array(test_data.test_id.values, dtype=int)
+    np.save(open(TEST_ID_FILE, 'wb'), test_id)
